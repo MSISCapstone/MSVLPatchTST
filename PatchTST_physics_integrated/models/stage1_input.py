7568ced541bfd80f4b4b98cc9c4f5b288cef2b89
Venkata Sai Mourya Teja Vuddandi <163160156+mvuddandi@users.noreply.github.com>
2026-01-27 09:23:19 +0530

    Update3 stage1_input.py

--- PatchTST_physics_integrated/models/stage1_input.py
+++ PatchTST_physics_integrated/models/stage1_input.py
@@ -2,18 +2,9 @@ import torch.nn as nn
 from PatchTST_physics_integrated.layers.revin import RevIN
 from PatchTST_physics_integrated.utils.time_features import add_hour_features
 
-
-#  PROPOSED CHANNEL GROUPING (DATA-DRIVEN)
-LONG_CHANNELS = [
-    0,1,2,3,4,5,6,7,8,9,10,
-    16,17,18,19
-]
-
-SHORT_CHANNELS = [
-    11,12,13,14,15
-]
-
-AUX_CHANNELS = [20, 21]
+LONG_CHANNELS  = [0,1,2,3,4,5,6,7,8,9,10,16,17,18,19]
+SHORT_CHANNELS = [11,12,13,14,15]
+AUX_CHANNELS   = [20,21]
 
 class Stage1Input(nn.Module):
     def __init__(self):
@@ -21,20 +12,6 @@ class Stage1Input(nn.Module):
         self.revin = RevIN(num_features=22)
 
     def forward(self, X_weather, hour_idx):
-        """
-        X_weather : [B, L, 20]
-        hour_idx  : [B, L]
-        """
-        # Add cyclic hour features
-        X = add_hour_features(X_weather, hour_idx)   # [B, L, 22]
-
-        # RevIN normalization
+        X = add_hour_features(X_weather, hour_idx)   # [B,L,22]
         X = self.revin(X)
-
-        # Channel grouping
-        X_long  = X[:, :, LONG_CHANNELS]
-        X_short = X[:, :, SHORT_CHANNELS]
-        X_aux   = X[:, :, AUX_CHANNELS]
-
-        return X_long, X_short, X_aux
-
+        return X[:, :, LONG_CHANNELS], X[:, :, SHORT_CHANNELS], X[:, :, AUX_CHANNELS]
