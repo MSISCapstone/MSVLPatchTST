de38ee7a5f6ed170285081be9f1a7b12daa9dc65
Venkata Sai Mourya Teja Vuddandi <163160156+mvuddandi@users.noreply.github.com>
2026-01-27 08:25:25 +0530

    Updated with cos & sin functions

--- PatchTST_physics_integrated/utils/time_features.py
+++ PatchTST_physics_integrated/utils/time_features.py
@@ -1,1 +1,15 @@
+import torch
+import math
+
+def add_hour_features(X, hour_idx):
+    """
+    X        : [B, L, C]
+    hour_idx : [B, L]  (0–23)
+    return   : [B, L, C+2]
+    """
+    hour_rad = 2 * math.pi * hour_idx.float() / 24.0
+    hour_sin = torch.sin(hour_rad).unsqueeze(-1)
+    hour_cos = torch.cos(hour_rad).unsqueeze(-1)
+
+    return torch.cat([X, hour_sin, hour_cos], dim=-1)
 
